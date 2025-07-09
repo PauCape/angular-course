bb2ea1d0cbb29525e2d4bae701aebec97791b36f
Pau Capellá Ballester <pau.capella@hotmail.es>
2025-07-09 16:43:51 +0200

    send parameters

--- 01-myApp/src/app/pages/dragonball-page/dragonball-page.component.ts
+++ 01-myApp/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -16,10 +16,17 @@ interface Character {
 })
 
 export class DragonballPageComponent {
+    name = signal('Nombre');
+    power = signal(0);
+
     characters = signal<Character[]>([
         {id: 1, name: 'Goku', power: 9001},
         {id: 2, name: 'Vegeta', power: 8000},
         {id: 3, name: 'Piccolo', power: 3000},
-        {id: 4, name: 'Yamcha', power: 500}
+        {id: 4, name: 'Yamcha', power: 500},
     ]);
+
+    addCharacter(){
+        console.log(this.name(), this.power());
+    }
 }
